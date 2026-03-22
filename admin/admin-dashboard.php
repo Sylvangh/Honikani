1aec20324366e200e197baeecbd672867ec5ba76
Sylvangh <sylvandoloritoyt@gmail.com>
2026-03-22 15:13:35 +0800

    Update admin-dashboard.php

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -1,7 +1,7 @@
 <?php
-ob_start(); // Start output buffering
+ob_start();
 
-// Prevent browser caching
+// Prevent caching
 header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
